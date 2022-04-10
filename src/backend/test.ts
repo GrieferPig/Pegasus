b995814fe960d129345b673cb39eb61c8a9057ba
GrieferPig <grieferpig@163.com>
2022-04-10 04:54:46 +0000

    Added Json Models

--- src/backend/test.ts
+++ src/backend/test.ts
@@ -7,7 +7,8 @@ import {JsonUtil} from "./util/JsonUtil"
 import {DetectEnv} from './env/DetectEnv'
 
 async function test(){
-    let _r:number = await new grabber().download("http://baidu.com",'C:/Users/REDACTED')
+    let usr = await new DetectEnv().getOsInfo()
+    let _r:number = await new grabber().download("http://baidu.com", usr.userHomeDir)
     console.log(_r)
     let _b:string = await new grabber().getRaw("https://bmclapi2.bangbang93.com/v1/packages/ed5d8789ed29872ea2ef1c348302b0c55e3f3468/1.7.10.json")
     console.log(await new JsonUtil().toJson(_b))
