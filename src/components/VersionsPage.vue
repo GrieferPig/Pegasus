aa1af877f5589c925d285936f85906893a8d7578
GrieferPig <grieferpig@163.com>
2022-05-04 20:42:21 +0800

    feat: added refresh gameList

--- src/components/VersionsPage.vue
+++ src/components/VersionsPage.vue
@@ -1,6 +1,6 @@
 <template>
 <v-list>
-  <v-btn v-on:click="readGameList">{{hasVer}}GamePath = {{ gamePath }}</v-btn>
+  <v-btn v-on:click="readGameList">REFRESH</v-btn>
   <v-list-subheader v-if="hasVer">Installed Versions</v-list-subheader>
   <v-list-subheader v-if="!hasVer">No valid installed versions detected.</v-list-subheader>
   <v-list-item
@@ -22,6 +22,46 @@ let gameList;
 let hasVer = false;
 let gamePath;
 
+function readGameList(){
+  backend.File.getGameFolder().then(path =>{
+    this.gamePath = path
+    let _gamePath = this.gamePath
+    backend.VersionMgr.listAllLocalVersions(this.gamePath)
+        .then(value => {
+          for(let i = 0; i<value.length;i++ ){
+            if(!backend.Game.validateGame(_gamePath, value[i])){
+              value.splice(i,1);
+              i--; // go back one item bcs we deleted one. Ugh, dynamic arrays are ANNOYING
+            }
+          }
+          this.gameList = value
+          this.hasVer = !!this.gameList; // lol type converting
+        })
+  })
+}
+
+// these two are basically same except the latter one isn't using * this *
+// there must be some workaround for this problem but im lazy
+function _read(){
+  backend.File.getGameFolder().then(path =>{
+    gamePath = path
+    let _gamePath = gamePath
+    backend.VersionMgr.listAllLocalVersions(gamePath)
+        .then(value => {
+          for(let i = 0; i<value.length;i++ ){
+            if(!backend.Game.validateGame(_gamePath, value[i])){
+              value.splice(i,1);
+              i--; // go back one item bcs we deleted one. Ugh, dynamic arrays are ANNOYING
+            }
+          }
+          gameList = value
+          hasVer = !!gameList; // lol type converting
+        })
+  })
+}
+
+_read()
+
 export default {
   name: "VersionsPage",
   data(){
@@ -31,25 +71,11 @@ export default {
         gamePath,
       }
   },
-  computed: {
-    readGameList: function(){
-      backend.File.getGameFolder().then(path =>{
-        this.gamePath = path
-        let _gamePath = this.gamePath
-        backend.VersionMgr.listAllLocalVersions(this.gamePath)
-            .then(value => {
-              for(let i = 0; i<value.length;i++ ){
-                if(!backend.Game.validateGame(_gamePath, value[i])){
-                  value.splice(i,1);
-                  i--; // go back one item bcs we deleted one. Ugh, dynamic arrays are ANNOYING
-                }
-              }
-              this.gameList = value
-              this.hasVer = !!this.gameList; // lol type converting
-              console.log(this.gameList+this.hasVer)
-            })
-      })
-    }
+  methods:{
+    readGameList
+  },
+  mounted() {
+    _read() // reload list when page reloaded
   }
 }
 </script>
