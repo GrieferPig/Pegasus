8a4e5351350cebc95196f01819209087bc813cb7
GrieferPig <grieferpig@163.com>
2022-06-21 22:33:14 +0800

    fix: fixed window dragger

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,14 @@
   <div class="background">
     <v-app :theme="theme" style="overflow: scroll !important;">
       <v-main>
-        <v-app-bar
-            color="primary"
-        >
+        <v-app-bar color="primary">
           <template v-slot:prepend>
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer" @click="sendNot"></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
           </template>
-
-          <v-app-bar-title class="appTitle" data-tauri-drag-region>{{ appTitle }}</v-app-bar-title>
-
+          <v-app-bar-title class="appTitle" data-tauri-drag-region style="display: block">{{
+              appTitle
+            }}
+          </v-app-bar-title>
           <template v-slot:append>
             <v-btn icon="mdi-dots-vertical"></v-btn>
           </template>
@@ -23,7 +22,7 @@
             <v-divider/>
             <div style="float:right;margin-bottom: 5px;margin-right: 5px">
               <v-btn color="transparent" icon flat @click="close" class="closeWinSwitch">
-                <v-icon icon="mdi-close" />
+                <v-icon icon="mdi-close"/>
               </v-btn>
               <v-btn color="transparent" icon flat @click="toggleTheme" class="toggleThemeSwitch">
                 <font-awesome-icon v-if="theme === 'darkTheme'" icon="moon"/>
@@ -52,6 +51,7 @@ import VersionsPage from "./components/VersionsPage.vue";
 import NavDrawer from './components/NavDrawer.vue'
 import {appWindow} from "@tauri-apps/api/window";
 
+const theme = ref('lightTheme')
 export default {
   name: 'App',
   components: {
@@ -69,17 +69,11 @@ export default {
       currentPage: 'LaunchPage',
     }
   },
-  setup() {
-    const theme = ref('lightTheme')
-    return {
-      theme,
-      toggleTheme: () => theme.value = theme.value === 'lightTheme' ? 'darkTheme' : 'lightTheme',
-    }
-  },methods: {
+  methods: {
     close() {
       appWindow.close()
     },
-    switchPage: function(pageName) {
+    switchPage: function (pageName) {
       let vm = this; //copy of this
       switch (pageName) {
         case "Launch":
@@ -97,7 +91,9 @@ export default {
         default:
           return;
       }
-    }
+    },
+    theme,
+    toggleTheme: () => theme.value = theme.value === 'lightTheme' ? 'darkTheme' : 'lightTheme',
   }
 }
 </script>
@@ -113,13 +109,8 @@ export default {
   justify-content: flex-start;
   text-overflow: ellipsis;
   overflow: hidden;
-  padding: 0px !important;
+  padding: 0 !important;
 }
-
-.v-toolbar-title {
-  padding: 6px 5px; /*why you need a 20px left padding*/
-}
-
 
 
 .background {
@@ -132,11 +123,14 @@ export default {
 
 
 .navDrawerStyle {
-  background: rgb(var(--v-theme-accent));
   border-color: transparent
 }
 
 body {
   overflow: hidden
+}
+
+.v-toolbar-title__placeholder {
+  pointer-events: none;
 }
 </style>
